1a77ddfd754c12b2f5ba9a7c534aa5e79218c9b2
User Name <username@MacBook.local>
2017-10-18 01:12:28 -0500

    moved group.py to modulde grouper.py

--- grouper.py
+++ grouper.py
@@ -2,5 +2,6 @@ def grouper(data, cluster_size=2):
     return [data[i:i+cluster_size] for i in range(0, len(data), cluster_size)]
 
 
-for i in xrange(1000000):
-   print "/".join(grouper("%06d" % i, 2))
+if __name__ == "__main__":
+   for i in xrange(1000000):
+      print "/".join(grouper("%06d" % i, 2))
